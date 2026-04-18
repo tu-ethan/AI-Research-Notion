950381e8675e34918455627d80342d5ab9c031ec
lolipop999 <ethan2.10101@gmail.com>
2026-04-18 09:23:00 -0400

    user input what to research

--- 10minAIAgent/main.py
+++ 10minAIAgent/main.py
@@ -4,8 +4,8 @@ from agent import get_agent_response
 
 load_dotenv()
 
-# query = input("What would you like to research? ")                
-response = get_agent_response("3 most interesting, actionable insight about self-improvement from Alex Hormozi online")
+query = input("What would you like to research? ")                
+response = get_agent_response(query)
 print(response)
 
 sendToNotion(response)
